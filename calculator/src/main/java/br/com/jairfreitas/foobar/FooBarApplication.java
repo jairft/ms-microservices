452c5723f97a38abb12542ec4dad58971724bcc0
jair-freitas <jair.freitas@ibm.com>
2023-06-03 09:03:44 -0300

    Projeto iniciado

---
+++ calculator/src/main/java/br/com/jairfreitas/foobar/FooBarApplication.java
@@ -0,0 +1,13 @@
+package br.com.jairfreitas.foobar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooBarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FooBarApplication.class, args);
+    }
+
+}
---
+++ calculator/src/test/java/br/com/jairfreitas/foobar/FooBarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jairfreitas.foobar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooBarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
